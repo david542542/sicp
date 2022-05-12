a4c726b3e56c822681bac82f8f9aa9213642af49
nouser <nomail>
2022-05-12 14:07:47 -0300

    Q&A 72119730 vanilla add,clear,remove

--- stackoverflow.com/72119730/crud-vanilla/public/Table.js
+++ stackoverflow.com/72119730/crud-vanilla/public/Table.js
@@ -1,4 +1,5 @@
 import { e, clear } from "./Dom.js"
+import * as Arr from "./Arr.js"
 
 // Data, persistent data structure, tailored to meet our app's needs
 function Data(name = "", elements = new Set) {
@@ -17,6 +18,13 @@ class Table {
     this.elem = elem
 		this.rows = rows
 		this.nameText = ""
+	}
+
+	splice(key) {
+		return func => {
+			this.rows = Arr.splice(this.rows, key, func)  // ⚠️
+			this.render() // ⚠️
+		}
 	}
 
 	onInput(event) {
@@ -42,13 +50,13 @@ class Table {
 					)
 				),
 				e("tbody", {},
-          this.rows.map(data => new Row(data).render()),
+          this.rows.map((data, key) => new Row(data, this.splice(key)).render()),
 					e("tr", {},
 						e("td", {}, null),
 						e("td", {}, null),
 						e("td", {}, null),
 						e("td", {},
-							e("input", {value: this.nameText, onInput: e => this.onInput(e) }),
+							e("input", {value: this.nameText, onInput: e => this.onInput(e)}),
 							e("button", {onClick: e => this.onCreate(e)}, "create set")
 						)
 					)
@@ -59,15 +67,35 @@ class Table {
 }
 
 class Row {
-  constructor(data) {
+  constructor(data, splice) {
     this.data = data
+		this.splice = splice
+		this.elemText = ""
   }
+	onInput(event) {
+		this.elemText = event.target.value // ⚠️
+	}
+	onAdd(event) {
+		this.splice(d => [d.add(this.elemText)]) // ⚠️
+	}
+	onClear(event) {
+		this.splice(d => [d.clear()]) // ⚠️
+	}
+	onRemove(event) {
+		this.splice(d => []) // ⚠️
+	}
   render() {
     return e("tr", {},
       e("td", {}, this.data.name),
       e("td", {}, this.data.size),
       e("td", {}, `{ ${Array.from(this.data.elements).join(" ")} }`),
-      e("td", {}, "...")
+      e("td", {},
+				e("input", {value: this.elemText, onInput: e => this.onInput(e)}),
+
+				e("button", {onClick: e => this.onAdd(e)}, "add"),
+				e("button", {onClick: e => this.onClear(e)}, "clear"),
+				e("button", {onClick: e => this.onRemove(e)}, "remove")
+			)
     )
   }
 }
