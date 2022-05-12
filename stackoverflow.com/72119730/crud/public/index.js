f3f0e2ad70b6b50978abbf30f2a7126feb5df35c
nouser <nomail>
2022-05-12 14:23:17 -0300

    convert tabs to spaces

--- stackoverflow.com/72119730/crud/public/index.js
+++ stackoverflow.com/72119730/crud/public/index.js
@@ -2,8 +2,8 @@ import { render } from "preact";
 import { Table, Data } from "./Table.js"
 
 const initialData = [
-	Data("fruits", new Set(["🍉","🍋","🥑"])),
-	Data("vegetables", new Set(["🥦","🌶","🌽","🧄"]))
+  Data("fruits", new Set(["🍉","🍋","🥑"])),
+  Data("vegetables", new Set(["🥦","🌶","🌽","🧄"]))
 ]
 
 render(<Table rows={initialData} />, document.querySelector("#app"))
